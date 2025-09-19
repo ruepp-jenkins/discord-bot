582e1b21c0f9b259160e136cdab86f2908d18b1f
MyUncleSam <12820919+MyUncleSam@users.noreply.github.com>
2025-09-19 13:41:22 +0200

    enable image build and push again

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,16 +7,16 @@ scripts/docker_initialize.sh
 cd source/DiscordBot
 
 # run build
-# echo "[${BRANCH_NAME}] Building images: ${IMAGE_FULLNAME}"
-# if [ "$BRANCH_NAME" = "master" ] || [ "$BRANCH_NAME" = "main" ]
-# then
-#     docker build \
-#         -t ${IMAGE_FULLNAME}:latest \
-#         -f Dockerfile \
-#         --push .
-# else
-#     docker build \
-#         -t ${IMAGE_FULLNAME}-test:${BRANCH_NAME} \
-#         -f Dockerfile \
-#         --push .
-# fi
+echo "[${BRANCH_NAME}] Building images: ${IMAGE_FULLNAME}"
+if [ "$BRANCH_NAME" = "master" ] || [ "$BRANCH_NAME" = "main" ]
+then
+    docker build \
+        -t ${IMAGE_FULLNAME}:latest \
+        -f Dockerfile \
+        --push .
+else
+    docker build \
+        -t ${IMAGE_FULLNAME}-test:${BRANCH_NAME} \
+        -f Dockerfile \
+        --push .
+fi
